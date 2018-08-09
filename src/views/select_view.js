f5b5fded7031dfa90f2b8d899106678501f63e0a
Gary Muir <garymuir17@gmail.com>
2018-08-09 14:41:04 +0100

    select view created and for each meothd created

--- src/views/select_view.js
+++ src/views/select_view.js
@@ -2,13 +2,24 @@ const PubSub = require("../helpers/pub_sub.js");
 
 const SelectView = function (container) {
   this.container = container;
+  this.monroes = null;
 };
 
 SelectView.prototype.bindEvents = function () {
   PubSub.subscribe("Monroes:all-data-ready", (evt) => {
     const monroes = evt.detail;
-    console.log(monroes);
+    this.populateDropDown(monroes);
+  });
+};
+
+SelectView.prototype.populateDropDown = function (monroes) {
+  monroes.forEach((monro, index) =>{
+    const option = document.createElement('option');
+    option.textContent = monro.region;
+    option.value = index;
+    this.container.appendChild(option);
   })
+
 };
 
 module.exports = SelectView;
