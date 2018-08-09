1f4d71f8728cf727cbc64f6fa17dc79dc64e651a
Gary Muir <garymuir17@gmail.com>
2018-08-09 13:52:41 +0100

    monro list view created

--- src/views/monro_list_view.js
+++ src/views/monro_list_view.js
@@ -1,5 +1,6 @@
 const PubSub = require("../helpers/pub_sub.js");
 
+
 const MonroListView = function (container) {
   this.container = container;
 };
@@ -7,8 +8,16 @@ const MonroListView = function (container) {
 MonroListView.prototype.bindEvents = function () {
   PubSub.subscribe('Monroes:all-data-ready', (evt) => {
     const monroes = evt.detail;
-    console.log(monroes);
+    this.render(monroes);
+
   });
+};
+
+MonroListView.prototype.render = function (monroes) {
+   monroes.forEach((monro) =>{
+     const monroView = new MonroView(this.container);
+     monroView.render();
+   });
 
 };
 
